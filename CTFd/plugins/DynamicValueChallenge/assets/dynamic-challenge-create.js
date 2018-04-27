0cf49b03b2989f6474f5cee4c58c42b2a87cf5f5
hatuw <jiaxi_wu@ieee.org>
2018-04-27 21:17:46 +0800

    fix bugs

--- CTFd/plugins/DynamicValueChallenge/assets/dynamic-challenge-create.js
+++ CTFd/plugins/DynamicValueChallenge/assets/dynamic-challenge-create.js
@@ -1,24 +1,48 @@
 // Markdown Preview
+// $('#desc-edit').on('shown.bs.tab', function (event) {
+//     var md = window.markdownit({
+//         html: true,
+//     });
+//     if (event.target.hash == '#desc-preview'){
+//         var editor_value = $('#desc-editor').val();
+//         $(event.target.hash).html(
+//             md.render(editor_value)
+//         );
+//     }
+// });
+// $('#new-desc-edit').on('shown.bs.tab', function (event) {
+//     var md = window.markdownit({
+//         html: true,
+//     });
+//     if (event.target.hash == '#new-desc-preview'){
+//         var editor_value = $('#new-desc-editor').val();
+//         $(event.target.hash).html(
+//             md.render(editor_value)
+//         );
+//     }
+// });
+// $("#solve-attempts-checkbox").change(function() {
+//     if(this.checked) {
+//         $('#solve-attempts-input').show();
+//     } else {
+//         $('#solve-attempts-input').hide();
+//         $('#max_attempts').val('');
+//     }
+// });
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Markdown Preview
 $('#desc-edit').on('shown.bs.tab', function (event) {
-    var md = window.markdownit({
-        html: true,
-    });
     if (event.target.hash == '#desc-preview'){
-        var editor_value = $('#desc-editor').val();
-        $(event.target.hash).html(
-            md.render(editor_value)
-        );
+        $(event.target.hash).html(marked($('#desc-editor').val(), {'gfm':true, 'breaks':true}));
     }
 });
 $('#new-desc-edit').on('shown.bs.tab', function (event) {
-    var md = window.markdownit({
-        html: true,
-    });
     if (event.target.hash == '#new-desc-preview'){
-        var editor_value = $('#new-desc-editor').val();
-        $(event.target.hash).html(
-            md.render(editor_value)
-        );
+        $(event.target.hash).html(marked($('#new-desc-editor').val(), {'gfm':true, 'breaks':true}));
     }
 });
 $("#solve-attempts-checkbox").change(function() {
@@ -33,3 +57,4 @@ $("#solve-attempts-checkbox").change(function() {
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
